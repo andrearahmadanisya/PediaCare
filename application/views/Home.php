5c45d9efbbdc9464315ac989a3e395339eb2d98c
rizkan03 <61906974+rizkan03@users.noreply.github.com>
2020-04-22 22:02:03 +0700

    Update Home.php

--- application/views/Home.php
+++ application/views/Home.php
@@ -5,46 +5,38 @@
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <!-- <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/css/login.css">  -->
-    <!-- <style>.carousel-inner > .item > img,.carousel-inner > .item > a > img { width: 80%; margin: auto;}</style> -->
-
-<style>
-  .carousel-inner img {
-      height: 500px;
-  }
-  .card{
-    padding:0px;
-  }
-</style>
+    <link rel="stylesheet" type="text/css" href="<?php base_url();?>assets/css/home.css"> 
+    
 
 </head>
 <body>
 
     <div class="card text-black" padding-left="0">
-      <img class="card-img" src="assets/images/home/bg_home2.png" alt="Card image" width="1200" height="400">
+      <img class="card-img" src="assets/images/home/bg_home2.png" alt="Card image" width="1200" height="300">
       <div class="card-img-overlay text-center">
-      <br><br><br><br><br>
+      <br><br><br>
         <h1 class="card-title">COVID-19 (NOVEL CORONAVIRUS)</h1>
         <p class="card-text">Mari kita cegah si kecil agar terhindar dari penyakit covid-19</p>
         <a class="btn btn-primary btn-lg" href="#" role="button">Read more</a>
       </div>
     </div>
-<br><br>
+
+    
     <div class="container">
-      <div class="row">
+      <div class="row ">
         <div class="col">
           <div class="thumbnail">
             <img src="assets/images/logo/1.png" width="400" height="300" class="img-responsive">
           </div>
         </div>
-        <div class="col">
+        <div class="col about">
           <h1>Apa itu Pediatricare?</h1>
           <p>Kidcare Children Therapy Center merupakan wadah untuk anak-anak dengan kebutuhan khusus dengan memberikan fasilitas yang lengkap dan penanganan yang tepat Kidcare diharapkan dapat membantu mengoptimalkan kemampuan yang dimiliki oleh anak-anak dengan kebutuhan khusus dan diharapkan mereka mampu untuk mandiri. <br>
           Kidcare didukung oleh segenap tenaga terapis yang berkompeten lulusan Fisioterapis dan sarjana Psikologi. Selain itu dalam upaya tetap meningkatkan kualitas terapi KIDCARE juga berkerjasama dengan konsultan terapy dan juga Psikologi.</p>
         </div>
       </div>
     </div>
-<br><br>
+
     <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
       <ul class="carousel-indicators">
         <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
@@ -72,45 +64,41 @@
       </a>
     </div>
 
-    <br><br>
-
     <div class="container">
       <div class="row">
-        <div class="col text-center">
-          <h1>Tips Seputar Kesehatan</h1>
+        <div class="col text-center tips">
+          <h2>Tips Seputar Kesehatan</h2>
         </div>
       </div>
     
-    <br><br><br>
-
       <div class="row">
         <div class="col">
-          <div class="card">
+          <div class="card kiri">
             <div class="card-body text-center" >
-              <h5 class="card-title">Card title</h5>
-              <p class="card-body">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="card-link">Card link</a>
+              <h5 class="card-title">Raising Healthy Kids</h5>
+              <p class="card-text">From safe sleep to fighting off the flu, our parent resources center provides smart tips
+              to help your baby, child, or teen grow and thrive</p>
+              <a href="#" class="card-link link-h">Learn more>></a>
             </div>
           </div>
         </div>
 
         <div class="col">
-          <div class="card">
+          <div class="card kanan" >
             <div class="card-body text-center" >
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="card-link">Card link</a>
+              <h5 class="card-title">Hope and Will Blog</h5>
+              <p class="card-text">In their own words, parents and caregiver share tips, tricks and firsthand esperiences
+              that are equal parts inspirational and educational</p>
+              <a href="#" class="card-link link-h">Learn more>></a>
             </div>
           </div>
         </div>
 
       </div>
     </div>
-
-
+  
 </body>
 </html>
 
-
-<br><br><br><br>
+<br><br>
 <footer>,</footer>
