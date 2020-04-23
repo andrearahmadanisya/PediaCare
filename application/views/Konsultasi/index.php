71911cdfed61b362b5230af4b5a419258f0bd503
andrearahmadanisya <61450795+andrearahmadanisya@users.noreply.github.com>
2020-04-24 00:24:44 +0700

    Update index.php

--- application/views/Konsultasi/index.php
+++ application/views/Konsultasi/index.php
@@ -1,7 +1,6 @@
 <head>
-<link rel="stylesheet" type="text/css" href="<?php echo base_url() . "assets/css/konsultasi.css"; ?>" />
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url() . "assets/css/konsultasi.css"; ?>" />
 
-</head>
 <body class="konsultasi">
     <img class="card-img" src="<?php echo base_url(); ?>assets/images/konsul/bg.png">
 
@@ -15,19 +14,19 @@
                 <h7 class="card-title"><b>Penanya : <?= $k['username'] ?></b></h7>
                 <br>
                 <!-- ini isi pertanyaan -->
-                <p class="card-text"><?= $k['question'] ?></p>
+                <p class="card-text"><?php echo substr($k['question'], 0, 50)  ?>...</p>
             </div>
             <div class="card-footer">
-                <a type="button" href="<?= base_url('lihat_konsultasi_controller'); ?>" class="btn btn-body"> lihat</a>
-                <a type="button" href="<?= base_url('JawabKonsultasiController'); ?>" class="btn btn-body">Jawab</a>
+                <a type="button" href="<?= base_url('lihat_konsultasi_controller/index/') . $k['id_konsultasi'] ?>" class="btn"> lihat</a>
+                <a type="button" href="<?= base_url('JawabKonsultasiController/index/') . $k['id_konsultasi'] ?>" class="btn">Jawab</a>
             </div>
         </div>
     <?php } ?>
-
     <br>
-    <div class="card-footer card-footer-tanya">
-        <a type="button" href="<?= base_url('InputKonsultasiController'); ?>" class="btn btn-tanya">Tanya!</a>
+    <div class="card-footer">
+        <a type="button" href="<?= base_url('InputKonsultasiController'); ?>" class="btn">Tanya!</a>
     </div>
+
     </table>
     <br></bbr>
 </body>
