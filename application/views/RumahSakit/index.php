d66a9b6f9ebe8e9bcf8b1af903f8379097e050b1
andrearahmadanisya <61450795+andrearahmadanisya@users.noreply.github.com>
2020-04-23 14:33:17 +0700

    Update index.php

--- application/views/RumahSakit/index.php
+++ application/views/RumahSakit/index.php
@@ -1,17 +1,33 @@
-<body>
-    <img class="card-img" src="<?php echo base_url(); ?>assets/images/konsul/bg.png">
-    
+<body class="rumahsakit">
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container text-center">
+            <h2 class="display-4">Anda Ingin Konsultasi Seputar Anak ?</h2>
+            <p class="lead">Tanyakan kepada kami secara mudah dan cepat</p>
+            <a class="btn btn-primary btn-lg" href="#" role="button">klik disini</a>
+        </div>
+    </div>
+
+    <br><br>
+
     <div class="container">
+        <h2>LIST RUMAH SAKIT</h2>
+        <br>
+        <div id="sidebar-wrapper">
+            <ul class="sidebar-nav float-right">
+                <img height="600px" width="300px" src="<?php echo base_url(); ?>assets/images/rs/sideimg.png">
+            </ul>
+        </div> <!-- /#sidebar-wrapper -->
         <?php foreach ($rumahsakit as $rs) { ?>
             <div class="row">
                 <div class="media position-relative">
+                    <img height="100px" aaclass="mr-3" src="assets/images/rs/icon.png" alt="Generic placeholder image">
                     <div class="media-body">
                         <i class="fa fa-hospital-o" style="font-size: 12px"></i>
-                        <h5 class="mt-0"><?= $rs['nama'] ?></h5>
+                        <h4 class="mt-0"><?= $rs['nama'] ?></h4>
                         <p><?= $rs['alamat'] ?></p>
                         <p><?= $rs['notelp'] ?></p>
-                        <a type="button" class="btn btn-danger btn-sm" href="<?php echo base_url() . 'RumahSakitController/delete/' . $rs['id_rumahsakit'] ?>" onClick="return confirm('Apakah Anda Yakin?')">Delete</a></td>
-                        <a type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#UpdateRS<?=$rs['id_rumahsakit']?>"> Update</a>
+                        <!-- <a type="button" class="btn btn-danger btn-sm" href="<?php echo base_url() . 'RumahSakitController/delete/' . $rs['id_rumahsakit'] ?>" onClick="return confirm('Apakah Anda Yakin?')">Delete</a></td>
+                        <a type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#UpdateRS<?= $rs['id_rumahsakit'] ?>"> Update</a> -->
                     </div>
                 </div>
             </div>
@@ -50,38 +66,38 @@
             </div>
         </div>
 
-    <?php foreach ($rumahsakit as $rs) {?>
-        <div class="modal fade" id="UpdateRS<?=$rs['id_rumahsakit']?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel1" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <center>
-                            <h2>EDIT DATA RUMAH SAKIT</h2>
-                        </center>
-                    </div>
-                    <div class="modal-body">
-                        <form method="POST" action="<?= base_url().'RumahSakitController/update/'.$rs['id_rumahsakit'] ?>">
-                            <div class="form-group">
-                                <label for="formGroupExampleInput">Nama Rumah Sakit</label>
-                                <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Nama Rumah Sakit" name="nama" value="<?=$rs['nama']?>" required>
-                            </div>
-                            <div class="form-group">
-                                <label for="formGroupExampleInput">Alamat</label>
-                                <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Alamat" name="alamat" value="<?=$rs['alamat']?>" required>
-                            </div>
-                            <div class="form-group">
-                                <label>No Telp</label><br>
-                                <input type="text" class="form-control" placeholder="Nomor Telp" name="notelp" value="<?=$rs['notelp']?>">
-                            </div>
-                            <div class="modal-footer">
-                                <input type="submit" class="btn btn-primary" id="hapus" value="Submit" placeholder="Simpan">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
-                            </div>
-                        </form>
+        <?php foreach ($rumahsakit as $rs) { ?>
+            <div class="modal fade" id="UpdateRS<?= $rs['id_rumahsakit'] ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel1" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <center>
+                                <h2>EDIT DATA RUMAH SAKIT</h2>
+                            </center>
+                        </div>
+                        <div class="modal-body">
+                            <form method="POST" action="<?= base_url() . 'RumahSakitController/update/' . $rs['id_rumahsakit'] ?>">
+                                <div class="form-group">
+                                    <label for="formGroupExampleInput">Nama Rumah Sakit</label>
+                                    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Nama Rumah Sakit" name="nama" value="<?= $rs['nama'] ?>" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="formGroupExampleInput">Alamat</label>
+                                    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Alamat" name="alamat" value="<?= $rs['alamat'] ?>" required>
+                                </div>
+                                <div class="form-group">
+                                    <label>No Telp</label><br>
+                                    <input type="text" class="form-control" placeholder="Nomor Telp" name="notelp" value="<?= $rs['notelp'] ?>">
+                                </div>
+                                <div class="modal-footer">
+                                    <input type="submit" class="btn btn-primary" id="hapus" value="Submit" placeholder="Simpan">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    <?php }?>
+        <?php } ?>
     </div>
 </body>
