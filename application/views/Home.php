9d80d242cb545e14cf6d5d9ae0cc3ca53d08256e
andrearahmadanisya <61450795+andrearahmadanisya@users.noreply.github.com>
2020-04-23 14:35:52 +0700

    Update Home.php

--- application/views/Home.php
+++ application/views/Home.php
@@ -1,103 +1,106 @@
-<html lang="en">   
+<html lang="en">
+
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1"> 
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="<?php base_url();?>assets/css/home.css"> 
-    
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="<?php base_url(); ?>assets/css/home.css">
+
 
 </head>
-<body>
 
-    <div class="card text-black" padding-left="0">
-      <img class="card-img" src="assets/images/home/bg_home2.png" alt="Card image" width="1200" height="300">
-      <div class="card-img-overlay text-center">
+<body class="home">
+
+  <div class="card text-black" padding-left="0">
+    <img class="card-img" src="assets/images/home/bg_home2.png" alt="Card image" width="1200" height="300">
+    <div class="card-img-overlay text-center">
       <br><br><br>
-        <h1 class="card-title">COVID-19 (NOVEL CORONAVIRUS)</h1>
-        <p class="card-text">Mari kita cegah si kecil agar terhindar dari penyakit covid-19</p>
-        <a class="btn btn-primary btn-lg" href="#" role="button">Read more</a>
-      </div>
+      <h1 class="card-title">COVID-19 (NOVEL CORONAVIRUS)</h1>
+      <p class="card-text">Mari kita cegah si kecil agar terhindar dari penyakit covid-19</p>
+      <a class="btn btn-primary btn-lg" href="#" role="button">Read more</a>
     </div>
+  </div>
 
-    
-    <div class="container">
-      <div class="row ">
-        <div class="col">
-          <div class="thumbnail">
-            <img src="assets/images/logo/1.png" width="400" height="300" class="img-responsive">
-          </div>
+
+  <div class="container">
+    <div class="row ">
+      <div class="col">
+        <div class="thumbnail">
+          <img src="assets/images/logo/1.png" width="400" height="300" class="img-responsive">
         </div>
-        <div class="col about">
-          <h1>Apa itu Pediatricare?</h1>
-          <p>Kidcare Children Therapy Center merupakan wadah untuk anak-anak dengan kebutuhan khusus dengan memberikan fasilitas yang lengkap dan penanganan yang tepat Kidcare diharapkan dapat membantu mengoptimalkan kemampuan yang dimiliki oleh anak-anak dengan kebutuhan khusus dan diharapkan mereka mampu untuk mandiri. <br>
+      </div>
+      <div class="col about">
+        <h1>Apa itu Pediatricare?</h1>
+        <p>Kidcare Children Therapy Center merupakan wadah untuk anak-anak dengan kebutuhan khusus dengan memberikan fasilitas yang lengkap dan penanganan yang tepat Kidcare diharapkan dapat membantu mengoptimalkan kemampuan yang dimiliki oleh anak-anak dengan kebutuhan khusus dan diharapkan mereka mampu untuk mandiri. <br>
           Kidcare didukung oleh segenap tenaga terapis yang berkompeten lulusan Fisioterapis dan sarjana Psikologi. Selain itu dalam upaya tetap meningkatkan kualitas terapi KIDCARE juga berkerjasama dengan konsultan terapy dan juga Psikologi.</p>
-        </div>
+      </div>
+    </div>
+  </div>
+
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ul class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ul>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="assets/images/home/slider_1.jpeg" class="d-block w-100" alt="..." width="500" height="300">
+      </div>
+      <div class="carousel-item">
+        <img src="assets/images/home/slider_1.jpeg" class="d-block w-100" alt="..." width="500" height="300">
+      </div>
+      <div class="carousel-item">
+        <img src="assets/images/home/slider_1.jpeg" class="d-block w-100" alt="..." width="500" height="300">
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+
+  <div class="container">
+    <div class="row">
+      <div class="col text-center tips">
+        <h2>Tips Seputar Kesehatan</h2>
       </div>
     </div>
 
-    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-      <ul class="carousel-indicators">
-        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-      </ul>
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img src="assets/images/home/slider_1.jpeg" class="d-block w-100" alt="..." width="500" height="300">
-        </div>
-        <div class="carousel-item">
-          <img src="assets/images/home/slider_1.jpeg" class="d-block w-100" alt="..." width="500" height="300">
-        </div>
-        <div class="carousel-item">
-          <img src="assets/images/home/slider_1.jpeg" class="d-block w-100" alt="..." width="500" height="300">
-        </div>
-      </div>
-      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
-    </div>
-
-    <div class="container">
-      <div class="row">
-        <div class="col text-center tips">
-          <h2>Tips Seputar Kesehatan</h2>
-        </div>
-      </div>
-    
-      <div class="row">
-        <div class="col">
-          <div class="card kiri">
-            <div class="card-body text-center" >
-              <h5 class="card-title">Raising Healthy Kids</h5>
-              <p class="card-text">From safe sleep to fighting off the flu, our parent resources center provides smart tips
+    <div class="row">
+      <div class="col">
+        <div class="card kiri">
+          <div class="card-body text-center">
+            <h5 class="card-title">Raising Healthy Kids</h5>
+            <p class="card-text">From safe sleep to fighting off the flu, our parent resources center provides smart tips
               to help your baby, child, or teen grow and thrive</p>
-              <a href="#" class="card-link link-h">Learn more>></a>
-            </div>
+            <a href="#" class="card-link link-h">Learn more>></a>
           </div>
         </div>
-
-        <div class="col">
-          <div class="card kanan" >
-            <div class="card-body text-center" >
-              <h5 class="card-title">Hope and Will Blog</h5>
-              <p class="card-text">In their own words, parents and caregiver share tips, tricks and firsthand esperiences
-              that are equal parts inspirational and educational</p>
-              <a href="#" class="card-link link-h">Learn more>></a>
-            </div>
-          </div>
-        </div>
-
       </div>
+
+      <div class="col">
+        <div class="card kanan">
+          <div class="card-body text-center">
+            <h5 class="card-title">Hope and Will Blog</h5>
+            <p class="card-text">In their own words, parents and caregiver share tips, tricks and firsthand esperiences
+              that are equal parts inspirational and educational</p>
+            <a href="#" class="card-link link-h">Learn more>></a>
+          </div>
+        </div>
+      </div>
+
     </div>
-  
+  </div>
+
 </body>
+
 </html>
 
 <br><br>
