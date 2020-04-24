76eeda8a9ba68e87086fade2ef0d3886101e4f2f
bagasmillena <62025719+bagasmillena@users.noreply.github.com>
2020-04-24 18:27:43 +0700

    jawab

--- application/models/KonsultasiModel.php
+++ application/models/KonsultasiModel.php
@@ -17,20 +17,13 @@ class KonsultasiModel extends CI_Model
 
     public function AddKonsultasi($data)
     {
-        // $data = [
-        //     "judul" => $this->input->post('judul', true),
-        //     "username" => $this->input->post('username', true),
-        //     "dokter" => $this->input->post('dokter', true),
-        //     "question" => $this->input->post('question', true),
-        //     "answer" => $this->input->post('answer', true)
-        // ];
         return $this->db->insert('konsultasi', $data);
     }
 
     public function UpdateKonsultasi($data, $id_konsultasi)
     {
         $this->db->where('id_konsultasi', $id_konsultasi);
-        return $this->db->update('id_konsultasi', $data);
+        return $this->db->update('konsultasi', $data);
     }
 
     public function DeleteKonsultasi($id_konsultasi)
@@ -38,7 +31,4 @@ class KonsultasiModel extends CI_Model
         return $this->db->delete('konsultasi', ['id_konsultasi' => $id_konsultasi]);
     }
 
-    public function jawab($id, $jawab)
-    {
-    }
 }
