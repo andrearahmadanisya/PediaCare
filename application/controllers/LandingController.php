d513fede3940a3369f16748aa5205233b532622d
Bagas <62025719+bagasmillena@users.noreply.github.com>
2020-04-25 07:38:01 +0700

    role

--- application/controllers/LandingController.php
+++ application/controllers/LandingController.php
@@ -5,13 +5,13 @@ class LandingController extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-//         $this->load->model('LandingModel');
         $this->load->library('form_validation');
     }
 
     public function index()
     {
         $data['judul'] = 'Landing';
+        $data['user'] = $this->session->userdata('user');
         $this->load->view('templates/header', $data);
         $this->load->view('landing');
         $this->load->view('templates/footer');
