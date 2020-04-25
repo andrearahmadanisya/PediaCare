fd14cc873f27943146a00b3a9a3f499caff1ceb3
Bagas <62025719+bagasmillena@users.noreply.github.com>
2020-04-25 07:44:39 +0700

    edit  tampilan

--- application/views/Konsultasi/inputPertanyaan.php
+++ application/views/Konsultasi/inputPertanyaan.php
@@ -1,22 +1,25 @@
-<body class="inputpertanyaan">
+<body class="jawabkonsultasi">
     <img class="card-img" src="<?php echo base_url(); ?>assets/images/konsul/bg.png">
     <br><br><br>
-
+    <?php $konsul = $konsultasi ?>
     <div class="card">
-        <form action="<?= base_url('InputKonsultasiController') ?>" method="POST">
+        <form action="<?= base_url('JawabKonsultasiController/index/') . $konsul['id_konsultasi'] ?>" method="POST">
+            <h4><b>Form Jawab</b></h4>
             <div class="form-group">
-                <label>Judul</label>
-                <input type="text" class="form-control" id="formGroupExampleInput" name="judul">
+                <h4><b><?= $konsul['judul'] ?></b></h4>
+                <p>Penanya :<?= $konsul['username'] ?></p>
+                <p class="card-text">Pertanyaan : <?= $konsul['question'] ?></p>
             </div>
             <div class="form-group">
-                <label for="formGroupExampleInput2">Pertanyaan</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="8" name="question"></textarea>
+                <label for="formGroupExampleInput2">Jawaban: <?= $konsul['answer'] ?></label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" name="jawaban" rows="8"></textarea>
             </div>
-            <left><button type="submit" class="btn btn-primary">Submit</button></left>
+            <div class="modal-footer">
+                <input type="submit" class="btn btn-primary" name="submit" value="Simpan">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+            </div>
         </form>
     </div>
-
-
     <br>
     <br>
     <br>
