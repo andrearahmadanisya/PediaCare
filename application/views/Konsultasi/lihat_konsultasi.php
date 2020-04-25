4b36fd6e0d3b70cbc724add71fbd03b9bce9c413
Bagas <62025719+bagasmillena@users.noreply.github.com>
2020-04-25 08:38:11 +0700

    role

--- application/views/Konsultasi/lihat_konsultasi.php
+++ application/views/Konsultasi/lihat_konsultasi.php
@@ -1,26 +1,37 @@
-<body class="lihat_konsultasi">
-  <div class="card">
-    <?php $konsul = $konsultasi ?>
-    <div class="card-header">
-      <h4><b><?= $konsul['judul'] ?></b></h4>
-      <p>penanya : <?= $konsul['username'] ?></p>
+
+
+<body class="inputpertanyaan">
+    <img class="card-img" src="<?php echo base_url(); ?>assets/images/konsul/bg.png">
+    <br><br><br>
+
+    <div class="card">
+        <?php if (!isset($user['fullname'])) { ?>
+            <div class="alert alert-danger" role="alert">
+                <strong>Anda belum Login. Silahkan login terlebih dahulu</strong>
+            </div>
+            <div>
+                <a class="btn navbar-dark my-2 my-sm-0" href="<?= base_url('LoginController'); ?>">Login</a>
+                or
+                <a class="btn navbar-dark my-2 my-sm-0" href="<?= base_url('RegistController'); ?>">Sign Up</a>
+            </div>
+        <?php } else { ?>
+            <form action="<?= base_url('InputKonsultasiController') ?>" method="POST">
+                <div class="form-group">
+                    <label>Judul</label>
+                    <input type="text" class="form-control" id="formGroupExampleInput" name="judul">
+                </div>
+                <div class="form-group">
+                    <label for="formGroupExampleInput2">Pertanyaan</label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="8" name="question"></textarea>
+                </div>
+                <left><button type="submit" class="btn btn-primary">Submit</button></left>
+            </form>
+        <?php } ?>
     </div>
-    <div class="card-body">
-      <p class="card-text"><?= $konsul['question'] ?></p>
-    </div>
-    <?php if($konsul['dokter'] and $konsul['answer']) { ?>
-    <div class="card-footer">
-      <p>Dijawab oleh : <?= $konsul['dokter'] ?></p>
-      <p class="card-jawab">Jawab : <?= $konsul['answer'] ?></p>
-    </div>
-    <?php } ?>
-    <?php if($user['role']==1) { ?>
-    <a type="button" href="<?= base_url('JawabKonsultasiController/index/') . $konsul['id_konsultasi'] ?>" class="btn">Jawab</a> 
-    <?php } ?>
+
+
     <br>
-    <left><a type="button" class="btn" href="<?= base_url('KonsultasiController'); ?>">back</a></left>
-  </div>
-  <br>
-  <br>
+    <br>
+    <br>
 </body>
 <footer>.</footer>
